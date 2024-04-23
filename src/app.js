9f6be93d7494701fcb6ea34994bb95aceccb080b
Mariano Cárcamo <marianocarcamo98@gmail.com>
2024-04-23 16:12:07 -0300

    feat: Actualizacion de app.js para incluir la api del carrito

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import productsRouter from './routes/products.router.js'
+import cartsRouter from './routes/carts.router.js'
 
 const PORT = 8080
 const app = express()
@@ -8,6 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/products', productsRouter)
+app.use('/api/carts', cartsRouter)
 
 app.listen(PORT, () => {
     console.log(`Server's up and running on port ${PORT}`)
